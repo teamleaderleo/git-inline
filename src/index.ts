cb8d870b4064e95b7a85356bc6771d2e1c2759cd
Leo <leoli_082000@hotmail.com>
2026-07-15 21:38:55 +0800

    revive: export the lean public API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-export type { CommitInfo } from './types/git-types';
-export { getFileHistory } from './github-api';
+export { getCommitDetails, getFileHistory } from './github-api';
+export { useGitHistory } from './hooks/use-git-history';
+export type {
+  UseGitHistoryOptions,
+  UseGitHistoryResult,
+} from './hooks/use-git-history';
 export { default as GitHistory } from './components/git-history';
+export type { GitHistoryProps } from './components/git-history';
 export { default as InlineGit } from './components/inline-git';
-export type { GitInlineConfig } from './utils/config-loader';
+export type { InlineGitProps } from './components/inline-git';
+export type {
+  CommitInfo,
+  GitAuthor,
+  GitCommitLoader,
+  GitCommitRequest,
+  GitFileChange,
+  GitHistoryLoader,
+  GitHistoryRequest,
+  GitHubCommitRequest,
+  GitHubHistoryRequest,
+} from './types/git-types';
